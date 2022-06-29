068d24cb279e46bb40d4865ea84367b0061eb29a
Earl Sioson <earlsioson@users.noreply.github.com>
2022-06-29 10:11:53 -0400

    use proper tag based on selection

--- {{cookiecutter.package_name}}/bundles/docker/scripts/e2g_run.sh
+++ {{cookiecutter.package_name}}/bundles/docker/scripts/e2g_run.sh
@@ -8,8 +8,10 @@ done
 
 if [ "$gpu_support" == "no" ]; then
     gpu_arg=""
+    tag=":osmesa"
 else
     gpu_arg="--gpus all"
+    tag=":egl"
 fi
 
-docker run -it --rm $gpu_arg -p 8080:80 -d {{cookiecutter.package_name}}
+docker run -it --rm $gpu_arg -p 8080:80 -d {{cookiecutter.package_name}}$tag
