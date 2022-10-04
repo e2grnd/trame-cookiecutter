97139fe0c5b042daca7c1541a2df122185d08242
Earl Sioson <earlsioson@users.noreply.github.com>
2022-10-04 08:57:03 -0400

    update with recent deployment changes RD-864

--- {{cookiecutter.package_name}}/{{cookiecutter.import_name}}/app/data.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.import_name}}/app/data.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import requests
@@ -7,8 +8,9 @@ def download(job_id):
 
     storage_host = os.environ.get("STORAGE_API_SERVICE_HOST", "storage")
     storage_port = os.environ.get("STORAGE_API_SERVICE_PORT", "8070")
-    url = f"http://{storage_host}:{storage_port}/e2g/download?name=trame/{{ cookiecutter.package_name }}/{job_id}.json"
-    r = requests.get(url)
+    url = f"http://{storage_host}:{storage_port}/internal/download?name=trame/tbreak/{job_id}.json"
+    auth_secret = os.environ.get("INTERNAL_AUTH_SHARED_SECRET")
+    r = requests.get(url, headers={"Authorization": auth_secret},)
 
     data = r.json()
     print("downloading from ....", url)
