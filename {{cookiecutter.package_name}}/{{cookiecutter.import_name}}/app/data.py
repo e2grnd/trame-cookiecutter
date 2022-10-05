e3bd4cdda5e8d6e7b728ff262bb32a2a07f95e36
Earl Sioson <earlsioson@users.noreply.github.com>
2022-10-05 13:01:04 -0400

    use sample cloud storage folder RD-864

--- {{cookiecutter.package_name}}/{{cookiecutter.import_name}}/app/data.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.import_name}}/app/data.py
@@ -8,7 +8,7 @@ def download(job_id):
 
     storage_host = os.environ.get("STORAGE_API_SERVICE_HOST", "storage")
     storage_port = os.environ.get("STORAGE_API_SERVICE_PORT", "8070")
-    url = f"http://{storage_host}:{storage_port}/internal/download?name=trame/tbreak/{job_id}.json"
+    url = f"http://{storage_host}:{storage_port}/internal/download?name=trame/vis/{job_id}.json"
     auth_secret = os.environ.get("INTERNAL_AUTH_SHARED_SECRET")
     r = requests.get(url, headers={"Authorization": auth_secret},)
 
