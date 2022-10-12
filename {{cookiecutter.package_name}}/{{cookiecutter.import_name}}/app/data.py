52352b45e1b25b884e3cb76f6a9ce8fa35b5254e
Aaron Lampros <alampros@gmail.com>
2022-10-12 09:27:29 -0400

    fix storage bucket name

--- {{cookiecutter.package_name}}/{{cookiecutter.import_name}}/app/data.py
+++ {{cookiecutter.package_name}}/{{cookiecutter.import_name}}/app/data.py
@@ -7,7 +7,7 @@ def download(job_id):
 
     storage_host = os.environ.get("STORAGE_API_SERVICE_HOST", "storage")
     storage_port = os.environ.get("STORAGE_API_SERVICE_PORT", "8070")
-    bucket_folder = os.environ.get("BUCKET_FOLDER", "dev")
+    bucket_folder = os.environ.get("BUCKET_FOLDER", "trame")
     url = f"http://{storage_host}:{storage_port}/internal/download?name={bucket_folder}/{{cookiecutter.package_name}}/{job_id}.json"
     auth_secret = os.environ.get("INTERNAL_AUTH_SHARED_SECRET")
     r = requests.get(
